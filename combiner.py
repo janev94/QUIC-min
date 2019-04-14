3d6c1be25a41ff188e6e6ec28db0793e1daee8ab
janev94 <misho.janew@gmail.com>
2019-04-14 16:01:32 +0000

    Batching combiner writes

--- combiner.py
+++ combiner.py
@@ -1,9 +1,23 @@
 import os
 
-with open('combined', 'a+') as sink:
-	for fname in os.listdir('res/'):
-		with open('res/'+fname) as f:
-			sink.write(f.read())
+root = os.environ('COMBINER_ROOT', '.')
+
+with open(root + '/combined_str', 'a+') as sink:
+	dir_list = os.listdir(root + '/res')
+	fraction = int(len(dir_list) / 10)
+	print fraction	
+	batch_write = ''
+	count = 1
+	for fname in dir_list:
+		with open(root + '/res/'+fname) as f:
+			batch_write += f.read()
+		if len(batch_write) % fraction == 0:
+			sink.write(batch_write)
+			batch_write = ''
+			print '%d%% written' % count
+			count += 1
+	if batch_write:
+		sink.write(batch_write)
 
 
 
