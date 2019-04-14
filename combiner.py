d032053a0d4bb9a776628b5c55e4d3ac9680b615
janev94 <misho.janew@gmail.com>
2019-04-14 23:01:55 +0000

    Combiner code did not update...

--- combiner.py
+++ combiner.py
@@ -2,22 +2,23 @@ import os
 
 root = os.environ.get('COMBINER_ROOT', '.')
 
-with open(root + '/combined', 'a+') as sink:
-	dir_list = os.listdir(root + '/res')
+with open(root + 'combined', 'a+') as sink:
+	dir_list = os.listdir(root + 'res')
 	fraction = int(len(dir_list) / 10)
 	print fraction	
-	batch_write = ''
-	count = 1
+	batch_write = []
+	written = 0
 	for fname in dir_list:
-		with open(root + '/res/'+fname) as f:
-			batch_write += f.read()
-		if len(batch_write) % fraction == 0:
-			sink.write(batch_write)
-			batch_write = ''
-			print '%d%% written' % count * 10
-			count += 1
+		with open(root + 'res/' + fname) as f:
+			batch_write.append(f.read())
+			written += 1
+		if written % fraction == 0:
+			sink.write(''.join(batch_write))
+			batch_write = []
+			print '%d%% written' % (len(dir)list / written 
+* 100)
 	if batch_write:
-		sink.write(batch_write)
+		sink.write(''.join(batch_write))
 
 
 
